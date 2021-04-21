d53506fe62be98d9bd29c56785c94755b9ea10ac
Johnny Kung <kjohnathan.engi@gmail.com>
2021-04-20 23:29:41 -0700

    change atlas db

--- databaseConnection.js
+++ databaseConnection.js
@@ -4,7 +4,7 @@ const is_heroku = process.env.IS_HEROKU || false;
 
 const databaseName = "lab_example"
 
-const herokuURI = "mongodb+srv://johnnykung:tzuning615@cluster0.4ulcc.mongodb.net/"+databaseName+"?retryWrites=true&w=majority"
+const herokuURI = "mongodb+srv://johnnykung:tzuning615@clusterokewebsite.a4z3c.gcp.mongodb.net/lab_example?retryWrites=true&w=majority"
 
 const localURI = "mongodb://localhost/"+databaseName+"?authSource=admin&retryWrites=true;"
 
